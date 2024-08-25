e1697fbf1d085ea1e0b27749cd99b05421b21b1b
ThousandCuts-0064 <92256555+ThousandCuts-0064@users.noreply.github.com>
2024-08-25 10:00:01 +0300

    removed ignored files

--- Neon.Web/wwwroot/ts/modules/args/active-connection-toggle-args.js
+++
@@ -1,9 +0,0 @@
-export default class ActiveConnectionToggleArgs {
-    username;
-    isActive;
-    constructor(username, isActive) {
-        this.username = username;
-        this.isActive = isActive;
-    }
-}
-//# sourceMappingURL=active-connection-toggle-args.js.map
